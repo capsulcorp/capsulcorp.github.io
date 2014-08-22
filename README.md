6b112f6645cced086cbc5ca590f21159c17b66a9
albert <aalquiso@gmail.com>
2014-08-21 21:26:06 -0700

    removed all deprecated files

--- README.md
+++
@@ -1,4 +0,0 @@
-capsul
-======
-
-A sorting social media app based on geolocation and time 
--- javascript/slideshow.js
+++
@@ -1,7 +0,0 @@
-var SlideView = (function() {
-  return {
-    render: function(item) {
-      $('#content-container').append(item);
-    }
-  }
-})();
